488476a350d132b8594e06d8244955144470dc41
virtuality0 <ayushjuyal246@gmail.com>
2025-03-25 14:45:52 +0530

    added cors setup

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 import express from "express";
 import { userRouter } from "./routes/user";
-import { auth } from "./middlewares/auth";
 import { contentRouter } from "./routes/content";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { brainRouter } from "./routes/brain";
+import cors from "cors";
 
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL ?? "");
 const app = express();
 
+app.use(cors());
 app.use(express.json()); // to be able to parse body in post requests
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/content", contentRouter);
